0d6f42590a8fb42c1e2759580cba209515ad4687
anand80 <anandratnam@live.com.my>
2016-12-19 15:12:59 +0800

    Add project files.

---
+++ EventExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public delegate void EventHandler();
+
+class Program
+{
+    public static event EventHandler _show;
+
+    static void Main()
+    {
+        // Add event handlers to Show event.
+        _show += new EventHandler(Dog);
+        _show += new EventHandler(Cat);
+        _show += new EventHandler(Mouse);
+        _show += new EventHandler(Mouse);
+
+        // Invoke the event.
+        _show.Invoke();
+    }
+
+    static void Cat()
+    {
+        Console.WriteLine("Cat");
+        Console.ReadLine();
+    }
+
+    static void Dog()
+    {
+        Console.WriteLine("Dog");
+        Console.ReadLine();
+    }
+
+    static void Mouse()
+    {
+        Console.WriteLine("Mouse");
+        Console.ReadLine();
+    }
+}
